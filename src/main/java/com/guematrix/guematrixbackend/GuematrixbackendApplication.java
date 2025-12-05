0d4eb16970c6b8a134f5f994428bff67bef9be4e
Dave <hadavidh@gmail.com>
2025-12-05 13:15:15 +0100

    premier commit appli guematrix

---
+++ src/main/java/com/guematrix/guematrixbackend/GuematrixbackendApplication.java
@@ -0,0 +1,13 @@
+package com.guematrix.guematrixbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuematrixbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuematrixbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guematrix/guematrixbackend/GuematrixbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guematrix.guematrixbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuematrixbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
